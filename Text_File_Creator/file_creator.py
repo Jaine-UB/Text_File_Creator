f07581a9ebb54b673b98626e13d9d711954b91e7
Unurjargal Boldbaatar <aja20.aja10success@gmail.com>
2025-09-22 12:20:01 -0400

    Second commit. Changed sentence structure.

--- Text_File_Creator/file_creator.py
+++ Text_File_Creator/file_creator.py
@@ -2,7 +2,7 @@ name = input("What's your name? \n")
 color = input("What's your favorite color? ")
 
 with open('example.txt', 'w') as file:
-    file.write(f"{name} created this file")
-    file.write(f"{color} created this file")
+    file.write(f"{name} created this file. \n")
+    file.write(f"{name}'s favorite color is {color}.")
 file.close()
 
